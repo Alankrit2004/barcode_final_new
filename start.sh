5c46af29aa41ffedd7a378da18f2bab4f0f491a9
Alankrit2004 <v.alankrit2004@gmail.com>
2025-02-28 12:06:39 +0530

    updated start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-/opt/render/project/src/.venv/bin/gunicorn -w 4 -b 0.0.0.0:8000 barcode_gen:app
+/opt/render/project/src/.venv/bin/gunicorn -w 4 -b 0.0.0.0:8000 barcode_gen_copy:app
 
